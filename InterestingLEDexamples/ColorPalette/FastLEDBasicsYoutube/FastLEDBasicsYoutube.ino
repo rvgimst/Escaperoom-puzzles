88dbc4a1216c9c068ee88a1bffc4bca1961d7b51
Ronald van Gimst <gimst@nlr.nl>
2022-05-17 21:44:30 -0400

    Added some palette examples from youtube tutorial

--- InterestingLEDexamples/ColorPalette/FastLEDBasicsYoutube/FastLEDBasicsYoutube.ino
+++ InterestingLEDexamples/ColorPalette/FastLEDBasicsYoutube/FastLEDBasicsYoutube.ino
@@ -36,12 +36,58 @@ DEFINE_GRADIENT_PALETTE( Sunset_Real_gp ) {
   198,  16,  0,130,
   255,   0,  0,160};
 
+// Gradient palette "bhw2_xc_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/bhw/bhw2/tn/bhw2_xc.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 28 bytes of program space.
+DEFINE_GRADIENT_PALETTE( bhw2_xc_gp ) {
+    0,   4,  2,  9,
+   58,  16,  0, 47,
+  122,  24,  0, 16,
+  158, 144,  9,  1,
+  183, 179, 45,  1,
+  219, 220,114,  2,
+  255, 234,237,  1};
+
+// Gradient palette "quagga_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/rc/tn/quagga.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 24 bytes of program space.
+DEFINE_GRADIENT_PALETTE( quagga_gp ) {
+    0,   1,  9, 84,
+   40,  42, 24, 72,
+   84,   6, 58,  2,
+  168,  88,169, 24,
+  211,  42, 24, 72,
+  255,   1,  9, 84};
+
+// Gradient palette "DEM_screen_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/td/tn/DEM_screen.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 24 bytes of program space.
+DEFINE_GRADIENT_PALETTE( DEM_screen_gp ) {
+    0,   0, 59,  5,
+   31,   3,156,  0,
+   63, 227,223, 33,
+  127, 227,131,  9,
+  191,  67, 32,  2,
+  255, 255,255,255};
+
+
+CRGBPalette16 paletteList[] = {DEM_screen_gp, quagga_gp, bhw2_xc_gp, Sunset_Real_gp, greenblue_gp, heatmap_gp, ForestColors_p};
+uint8_t NUM_PALETTES = 7;
+uint8_t currentPaletteIndex = 0;
 CRGBPalette16 currentPalette;
 TBlendType    currentBlending;
 uint8_t paletteIndex = 0;
 uint8_t colorIndex[NUM_LEDS];
+int paletteDuration = 10; // duration in [s] each palette should be shown
 
 void setup() {
+  // Start a serial connection
+  Serial.begin(9600);
+  Serial.println(__FILE__ " Created:" __DATE__ " " __TIME__);
+  
   FastLED.clear();
   delay( 3000 ); // power-up safety delay
   FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
@@ -51,26 +97,33 @@ void setup() {
 
 //THIRD TEST
 // Fill the colorIndex array with random numbers
-//  for (int i=0; i<NUM_LEDS; i++) {
-//    colorIndex[i] = random8();
-//  }
+  for (int i=0; i<NUM_LEDS; i++) {
+    colorIndex[i] = random8();
+  }
+
+  // for looping through different palettes
+  currentPaletteIndex = 0;
+  currentPalette = paletteList[currentPaletteIndex];
 
   FastLED.clear();
 }
 
 void loop() {
-// comment out one ot the tests below
+  ChangePalettePeriodically();
+// comment out one of the tests below
 
 //FIRST TEST
-  // 
-  currentPalette = Sunset_Real_gp; //heatmap_gp; // choose palette
-  fill_palette(leds, NUM_LEDS, paletteIndex, 255/NUM_LEDS, currentPalette, 255, currentBlending);
-  EVERY_N_MILLISECONDS(50) {
-    paletteIndex++;
-  }
+  // Running palette, decrease ms to increase speed
+////  currentPalette = Sunset_Real_gp; //heatmap_gp; // choose palette
+//  fill_palette(leds, NUM_LEDS, paletteIndex, 255/NUM_LEDS, currentPalette, 255, currentBlending);
+//  EVERY_N_MILLISECONDS(50) {
+//    paletteIndex++;
+//  }
 
 //SECOND TEST
-//  currentPalette = greenblue_gp; //ForestColors_p;
+// set random LEDs to a random palette color with full brightness. Fade the whole LED strip to black every loop
+// This causes several LEDs to turn dark, depending on dark colors in the palette
+////  currentPalette = greenblue_gp; //ForestColors_p;
 //  EVERY_N_MILLISECONDS(1) {
 //    // Switch on an LED at random, choosing a random color from a palette
 //    uint8_t r=random8(0, NUM_LEDS - 1);
@@ -80,16 +133,35 @@ void loop() {
 //  fadeToBlackBy(leds, NUM_LEDS, 1);
 
 //THIRD TEST
-//  currentPalette = Sunset_Real_gp;
-//  // Color each pixel from the palette using the index from colorIndex[]
-//  for (int i=0; i<NUM_LEDS; i++) {
-//    leds[i] = ColorFromPalette(currentPalette, colorIndex[i]);
-//  }
-//  EVERY_N_MILLISECONDS(1) {
-//    for (int i=0; i<NUM_LEDS; i++) {
-//      colorIndex[i]++;
-//    }
-//  }
+// set every LED to a random color of the palette, then shift each individual LED color 1 in the palette
+  //currentPalette = greenblue_gp; //Sunset_Real_gp;
+  // Color each pixel from the palette using the index from colorIndex[]
+  for (int i=0; i<NUM_LEDS; i++) {
+    leds[i] = ColorFromPalette(currentPalette, colorIndex[i]);
+  }
+  EVERY_N_MILLISECONDS(15) {
+    for (int i=0; i<NUM_LEDS; i++) {
+      colorIndex[i]++;
+    }
+  }
 
   FastLED.show();
+}
+
+void ChangePalettePeriodically() {
+  static unsigned long lastUpdate = millis();
+  unsigned long currentTime = millis();
+  unsigned long deltaT = currentTime - lastUpdate;
+
+  if (deltaT >= paletteDuration * 1000) {
+    currentPaletteIndex++;
+
+    if (currentPaletteIndex == NUM_PALETTES) {
+      currentPaletteIndex = 0; // reset to beginning of palette list
+    }
+    currentPalette = paletteList[currentPaletteIndex];
+    Serial.println(" pi:");
+    Serial.println(currentPaletteIndex);
+    lastUpdate = currentTime;
+  }
 }
