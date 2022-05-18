6b511281528d30b5cb8a761b579fb7191e61b124
Ronald van Gimst <gimst@nlr.nl>
2022-05-17 21:43:51 -0400

    Filled trackLED puzzle with values for Escape From the Past puzzle

--- LEDTrack_Puzzle/LEDTrack_Puzzle.ino
+++ LEDTrack_Puzzle/LEDTrack_Puzzle.ino
@@ -15,20 +15,25 @@
  * of the LED on the strip, which is how they are addressed in the leds array. The scale along the top is the
  * "distance travelled" along the track to reach that point.
  *
+ * ==== LAYOUT ESCAPE FROM THE PAST, MD, US ====
  * Distance Travelled
- *  0   1   2   3   4   5   6   7    8    9    10   11   12   13   14   15   16   17   18   19   20   21   22   23   24   25   26   27   28   29   30   31   32   33   34   35   36   37
- *                                                                                                                                                                              (Track 4)
- *                                                                     [25] [26] [27] [28] [29] [30] [31] [32] [33] [34] [35] [36] [37] [38] [39] [40] [41] [42] [43] [44] [45] [46] [47]
- *                                                                    /
- *                             [12] [13] [14] [15] [16] [17] [18] [19] (Switch 3)
- *                            /                                       \               (Track 3)                               (Track 2)
- *                           /                                         [20] [21] [22] [23] [24]                          [50] [49] [48]
- * [0] [1] [2] [3] [4] [5] [6] (Switch 0)                                                                               /
- *                           \                                    [61] [60] [59] [58] [57] [56] [55] [54] [53] [52] [51] (Switch 2)
- *                            \                                  /                                                      \                    (Track 1)
- *                             [11] [10]  [9]  [8]  [7] [63] [62] (Switch 1)                                             [92] [91] [90] [89] [88] [87]
- *                                                               \                                                                                                         (Track 0)
- *                                                                [64] [65] [66] [67] [68] [69] [70] [71] [72] [73] [74] [75] [76] [77] [78] [79] [80] [81] [82] [83] [84] [85] [86]
+ *  0   1   2   3    4    5    6    7    8    9    10   11   12   13   14   15   16   17   18   19   20   21   22   23   24   25   26   27   28   29   30   31   32   33   34   35   36   37
+ *                                                                                             (Track 4)
+ *                            [6]  [7]  [8]  [9]  [10] [11] [12] [13] [14] [15] [16] [17] [18] [19] [20] 
+ *                          /
+ *                  [4]  [5] (Switch 1)
+ *                 /        \                                                                  (Track 3)
+ *                /           [35] [34] [33] [32] [31] [30] [29] [28] [27] [26] [25] [24] [23] [22] [21]
+ * [0] [1] [2] [3] (Switch 0)                                                                                                                         (Track 2)
+ *               |                                                                        [50] [51] [52] [53] [54] [55] [56] [57] [58] [59] [60] [61] [62] [63]
+ *               |                                                                       /
+ *                \                                    [43] [44] [45] [46] [47] [48] [49] (Switch 3)
+ *                 \                                  /                                  \                              (Track 1)
+ *                  |                                /                                    [64] [65] [66] [67] [68] [69] [70] [71]
+ *                  [36] [37] [38] [39] [40] [41] [42] (Switch 2)                                            
+ *                                                    \               (Track 0)
+ *                                                     [76] [75] [74] [73] [72] 
+ *
  */
 
 // INCLUDES
@@ -41,7 +46,7 @@
 #define COLOR_ORDER GRB
 #define BRIGHTNESS  64
 // The total number of LEDs across all track segments
-#define NUM_LEDS 93
+#define NUM_LEDS 77
 // The greatest number of LEDs on any given track down which a particle can travel
 #define MAX_NUM_LEDS_PER_TRACK 40
 // for palette on solving the puzzle
@@ -74,28 +79,28 @@ const byte switchPins[numSwitchPins] = { 2, 3, 4, 5 };
 // The maximum number of particles that will be alive at any one time
 const byte maxParticles = 10;
 // Number of milliseconds between each particle being spawned
-const int _rate = 5000;
+const int _rate = 4000;
 // How many different types of particle are there?
 const byte numParticleTypes = 5;
 // Define a colour associated with each type of particle
-const CRGB colours[numParticleTypes] = { CRGB::Red, CRGB::Green, CRGB::Yellow, CRGB::Blue, CRGB::White };
+const CRGB colours[numParticleTypes] = { CRGB::White, CRGB::Red, CRGB::Blue, CRGB::Yellow, CRGB::Green};
 // Specify the order in which LEDs are traversed down each possible track from start to finish. -1 indicates beyond the end of the track
 const unsigned int ledTrack[numParticleTypes][MAX_NUM_LEDS_PER_TRACK] = {
   // LHS
-  {0,  1,  2,  3,  4,  5,  6, 11, 10,  9,  8,  7, 63, 62, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, -1, -1, -1},
-  {0,  1,  2,  3,  4,  5,  6, 11, 10,  9,  8,  7, 63, 62, 61, 60, 59, 58, 57, 56, 55, 54, 53, 52, 51, 92, 91, 90, 89, 88, 87, -1, -1, -1, -1, -1, -1, -1, -1, -1},
-  {0,  1,  2,  3,  4,  5,  6, 11, 10,  9,  8,  7, 63, 62, 61, 60, 59, 58, 57, 56, 55, 54, 53, 52, 51, 50, 49, 48, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1},
-  {0,  1,  2,  3,  4,  5,  6, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1},
-  {0,  1,  2,  3,  4,  5,  6, 12, 13, 14, 15, 16, 17, 18, 19, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, -1, -1}
+  {0,  1,  2,  3, 36, 37, 38, 39, 40, 41, 42, 76, 75, 74, 73, 72, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1},
+  {0,  1,  2,  3, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 64, 65, 66, 67, 68, 69, 70, 71, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1},
+  {0,  1,  2,  3, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, -1, -1, -1, -1, -1, -1, -1, -1},
+  {0,  1,  2,  3,  4,  5, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1},
+  {0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}
   // RHS
 };
 // Specify which LEDs should be used as a score counter for each track
 const unsigned int scoreLEDs[numParticleTypes][3] = {
-  {86, 85, 84},
-  {87, 88, 89},
-  {48, 49, 50},
-  {24, 23, 22},
-  {47, 46, 45}
+  {72, 73, 74},
+  {71, 70, 69},
+  {63, 62, 61},
+  {21, 22, 23},
+  {20, 19, 18}
 };
 
 // GLOBALS
@@ -173,7 +178,7 @@ void spawnParticle(){
     if(!particlePool[i].alive){
       // Reset it as a new particle
       particlePool[i].position = 0;
-      particlePool[i].speed = 1;
+      particlePool[i].speed = 2;//10; // between 1-16
       particlePool[i].type = random(0, 5);
       particlePool[i].length = particlePool[i].type + 1;
       particlePool[i].track = 0;
@@ -255,7 +260,6 @@ void onSolve() {
 
 
 void loop() {
-
   // For debug use only, print out the state of each toggle switch
   Serial.print("status switches: ");
   for(int i=0; i<numSwitchPins; i++){
@@ -283,20 +287,22 @@ void loop() {
     // Only if it's alive!
     if(particlePool[i].alive){
 
+      // RVG: dont forget to change these if track layout changes
+      // RVG: use the LED index of the switch locations (where the tracks split)
       // If the particle is on one of the switch points, change track as appropriate
-      if(particlePool[i].position/16 == 6) {
+      if(particlePool[i].position/16 == 3) {
         particlePool[i].track = (digitalRead(switchPins[0])) ? 0 : 3;
       }
-      // If we took a left at the first junction then we come across the next switch after 13 LEDs
-      if(particlePool[i].position/16 == 13 && particlePool[i].track < 2) {
+      // If we took a left at the first junction then we come across the next switch after in total 10 LEDs
+      if(particlePool[i].position/16 == 10 && particlePool[i].track < 3) {
         particlePool[i].track = (digitalRead(switchPins[1])) ? 0 : 1;
       }
-      // If we took a right at the first junction then we come aross the next switch after 14 LEDs
-      if(particlePool[i].position/16 == 14 && particlePool[i].track >= 2) {
+      // If we took a right at the first junction then we come aross the next switch after 5 LEDs
+      if(particlePool[i].position/16 == 5 && particlePool[i].track >= 3) {
         particlePool[i].track = (digitalRead(switchPins[2])) ? 3 : 4;
       }
-      // If we went left, then right, there's a final junction after 24 LEDs
-      if(particlePool[i].position/16 == 24 && particlePool[i].track == 1) {
+      // If we went left, then right, there's a final junction after 17 LEDs
+      if(particlePool[i].position/16 == 17 && particlePool[i].track == 1) {
         particlePool[i].track = (digitalRead(switchPins[3])) ? 1 : 2;
       }
 
@@ -316,7 +322,8 @@ void loop() {
         // The particle ended in the wrong track
         else if(particlePool[i].type != particlePool[i].track && score[particlePool[i].track] > 0) {
           // Reduce score for this track
-          score[particlePool[i].track]--;
+          // RVG commented out for now - so no degrading of the score
+          //score[particlePool[i].track]--;
         }
       }
 
@@ -335,7 +342,7 @@ void loop() {
     }
     totalScore += score[i];
   }
-  if(totalScore == 0 /*15 RVG changed for testing*/) {
+  if(totalScore == 15 /*15 RVG changed for testing*/) {
     onSolve();
   }
  
