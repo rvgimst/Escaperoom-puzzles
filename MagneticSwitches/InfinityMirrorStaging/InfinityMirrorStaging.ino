34a07cc8044257ae637f9f53cb3df73fa0197938
Ronald van Gimst <gimst@nlr.nl>
2022-09-20 20:48:57 -0400

    tried out palette blending

--- MagneticSwitches/InfinityMirrorStaging/InfinityMirrorStaging.ino
+++ MagneticSwitches/InfinityMirrorStaging/InfinityMirrorStaging.ino
@@ -16,6 +16,39 @@ int patternPosition = 0; //for simple shifting of LEDs
 int blendrate = 0;
 int blenddirection = 1;
 
+// Gradient palette "bhw1_07_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/bhw/bhw1/tn/bhw1_07.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 8 bytes of program space.
+DEFINE_GRADIENT_PALETTE( Yellow_gp ) {
+    0, 232, 65,  1,
+  255, 229,227,  1};
+
+// Gradient palette "bath_112_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/esri/hypsometry/bath/tn/bath_112.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 508 bytes of program space.
+DEFINE_GRADIENT_PALETTE( Blue_gp ) {
+    0,   0,  3, 34,
+   70,   0, 18,123,
+  157,   3, 81,255,
+  192,  20,124,255,
+  216,  51,162,255,
+  255, 120,209,255};
+
+// Gradient palette "girlcat_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/rc/tn/girlcat.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 20 bytes of program space.
+DEFINE_GRADIENT_PALETTE( Green_gp ) {
+    0, 173,229, 51,
+   73, 139,199, 45,
+  140,  46,176, 37,
+  204,   7, 88,  5,
+  255,   0, 24,  0};
+
+uint8_t paletteIndex = -1;
+
 void setup() {
   // Start a serial connection
   Serial.begin(9600);
@@ -29,7 +62,30 @@ void setup() {
   FastLED.show();
 }
 
+CRGBPalette16 currentPalette(Yellow_gp);
+CRGBPalette16 targetPalette(Yellow_gp);
+uint8_t whichPalette = 0;
+
+void RunPulsatingFastLEDsUsingPalette() {
+  fill_palette(fastleds, NUM_FLEDS, paletteIndex, 255/NUM_FLEDS, currentPalette, BRIGHTNESS, LINEARBLEND);
+
+  // create a sine wave with period of 2 sec (30bpm) to change brightness of the strip
+  // and one with 20bpm
+  // beatsin8(bpm, minvalue, maxvalue, phase offset, timebase
+  uint8_t sinBeat1 = beatsin8(30, 0, 64, 0, 0);
+  uint8_t sinBeat2 = beatsin8(60, 0, 64, 0, 0);
+  
+  FastLED.setBrightness((sinBeat1 + sinBeat2)/2);
+
+  EVERY_N_MILLISECONDS(10) {
+//    paletteIndex++;
+  }
+
+  FastLED.show();
+}
+
 void StartPulsatingFastLEDs() {
+  FastLED.setBrightness(BRIGHTNESS);
   // own pattern of LEDs: red every 5 LEDS, others blue
   for (int i=0; i<NUM_FLEDS; i++) {
     if (i % numLEDsInPattern == 0) {
@@ -72,6 +128,7 @@ void StartPulsatingFastLEDs() {
 uint8_t speed = 3; // between 1-16
 
 void StartRotatingFastLEDS0() {
+  FastLED.setBrightness(BRIGHTNESS);
   EVERY_N_MILLISECONDS(10) {
     // advance pattern 1 step. (16 steps corresponds to 1 complete pixel shift)
     patternPosition += speed;
@@ -97,6 +154,7 @@ void StartRotatingFastLEDS0() {
 }
 
 void StartRotatingFastLEDS1() {
+  FastLED.setBrightness(BRIGHTNESS);
   EVERY_N_MILLISECONDS(10) {
     // advance pattern 1 step. (16 steps corresponds to 1 complete pixel shift)
     patternPosition += speed;
@@ -126,6 +184,7 @@ byte hue = 0;
 
 // using multiple offset sawtooth waves to create running LED effect
 void StartRotatingFastLEDS2(bool dohue=true) {
+  FastLED.setBrightness(BRIGHTNESS);
   for (byte i=0; i<10; i++) {
     pos[i] = map(beat8(20, i*300), 0, 255, 0, NUM_FLEDS - 1);
     if (dohue) {
@@ -153,6 +212,7 @@ byte d = numLEDsInPattern / 3; // divider from bright to dim LEDs in pattern
 #define BRI_VAL 200
 
 void StartRotatingFastLEDS3() {
+  FastLED.setBrightness(BRIGHTNESS);
   EVERY_N_MILLISECONDS(10) {
     // advance pattern 1 step. (16 steps corresponds to 1 complete pixel shift)
     patternPosition += speed;
@@ -166,13 +226,13 @@ void StartRotatingFastLEDS3() {
       byte ledidx = (i + patternPosition/16) % NUM_FLEDS;
        
       if (i % numLEDsInPattern == 0) { // bright pixel in front of dim turning dim
-        fastleds[ledidx] = blend(CHSV(HUE_BLUE, 255, BRI_VAL), CHSV(HUE_GREEN, 255, DIM_VAL), frac*16);
+        fastleds[ledidx] = blend(CHSV(HUE_BLUE, 255, BRI_VAL), CHSV(HUE_BLUE, 255, DIM_VAL), frac*16);
       } else if (i % numLEDsInPattern == d){ // dim pixel in front of bright turning bright
-        fastleds[ledidx] = blend(CHSV(HUE_GREEN, 255, DIM_VAL), CHSV(HUE_BLUE, 255, BRI_VAL), frac*16);
+        fastleds[ledidx] = blend(CHSV(HUE_BLUE, 255, DIM_VAL), CHSV(HUE_BLUE, 255, BRI_VAL), frac*16);
       } else if (i % numLEDsInPattern < d) { // bright pixels 
         fastleds[ledidx] = CHSV(HUE_BLUE, 255, BRI_VAL);
       } else { // dim pixels
-        fastleds[ledidx] = CHSV(HUE_GREEN, 255, DIM_VAL);
+        fastleds[ledidx] = CHSV(HUE_BLUE, 255, DIM_VAL);
       }
     }
     FastLED.show();
@@ -185,6 +245,7 @@ void StartColorBlend() {
   CRGB col2 = CRGB::Red;
   CRGB blendcol;
 
+  FastLED.setBrightness(BRIGHTNESS);
 // to test how col1 and col2 look like
 //  fill_solid(fastleds, NUM_FLEDS, col1);
 //  FastLED.show();
@@ -228,6 +289,32 @@ void ResetFastLEDs() {
 byte state = 0;
 
 void loop() {
+  // this loop tests fade between 2 pulsating effects where colors from 1 blend into the other
+  RunPulsatingFastLEDsUsingPalette();
+
+  EVERY_N_SECONDS(10) {
+    switch (whichPalette) {
+      case 0:
+        targetPalette = Blue_gp;
+        Serial.println("switching to Blue...");
+        break;
+      case 1:
+        targetPalette = Green_gp;
+        Serial.println("switching to Green...");
+        break;
+      case 2:
+        targetPalette = Yellow_gp;
+        Serial.println("switching to Yellow...");
+        break;
+    }
+    
+    whichPalette++;
+    if (whichPalette > 2) whichPalette = 0;
+  }
+  nblendPaletteTowardPalette( currentPalette, targetPalette, 20);
+}
+
+void loop1() {
   switch (state) {
     case 0:
       StartPulsatingFastLEDs();
@@ -236,23 +323,29 @@ void loop() {
       StartColorBlend();
       break;
     case 2:
-      StartRotatingFastLEDS0();
+      StartRotatingFastLEDS0(); // 1 red LED in pattern
       break;
     case 3:
-      StartRotatingFastLEDS1();
+      StartRotatingFastLEDS1(); // 2 red LEDs in pattern
       break;
     case 4:
-      StartRotatingFastLEDS2(false);
+      StartRotatingFastLEDS2(false); // sawtooth waves repeated
       break;
     case 5:
-      StartRotatingFastLEDS2(true);
+      StartRotatingFastLEDS2(true); // sawtooth waves repeated
       break;
     case 6:
-      StartRotatingFastLEDS3();
+      // change brightness using fixed pattern
+      // Num LEDs in pattern = NUM_FLEDS/RINGDIVIDER
+      // first 1/3 of pixels in pattern: bright
+      // last 2/3 of pixels in pattern: dim
+      StartRotatingFastLEDS3(); // 
       break;
   }
   
   EVERY_N_SECONDS(15) {
+    ResetFastLEDs();
+    delay(500);
     state++;
     if (state == NUM_STATES) state = 0;
   }
